9d515f372e2832db1126ea5b3b76a28468719389
B. de Souza Silva <b.de.souza.silva@avanade.com>
2020-04-02 23:21:47 -0300

    test

--- src/main/java/br/com/dev/eficiente/nosso/mercado/livre/NossoMercadoLivreApplication.java
+++ src/main/java/br/com/dev/eficiente/nosso/mercado/livre/NossoMercadoLivreApplication.java
@@ -8,6 +8,7 @@ public class NossoMercadoLivreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NossoMercadoLivreApplication.class, args);
+        System.out.println("Test");
     }
 
 }
