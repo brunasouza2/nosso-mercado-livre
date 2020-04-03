0a8d187e8de4f6cc795e2018162716c953e67f79
B. de Souza Silva <b.de.souza.silva@avanade.com>
2020-04-02 23:03:16 -0300

    Initial Commit

---
+++ src/main/java/br/com/dev/eficiente/nosso/mercado/livre/NossoMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.eficiente.nosso.mercado.livre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoMercadoLivreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NossoMercadoLivreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dev/eficiente/nosso/mercado/livre/NossoMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.eficiente.nosso.mercado.livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoMercadoLivreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
