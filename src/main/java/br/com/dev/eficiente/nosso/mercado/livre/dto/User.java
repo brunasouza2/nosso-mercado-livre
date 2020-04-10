e4ff8f1256019966344c3c82b2e765b9f450ba12
B. de Souza Silva <b.de.souza.silva@avanade.com>
2020-04-09 22:38:12 -0300

    Ajustes da review

--- src/main/java/br/com/dev/eficiente/nosso/mercado/livre/dto/User.java
+++ src/main/java/br/com/dev/eficiente/nosso/mercado/livre/dto/User.java
@@ -1,23 +1,20 @@
 package br.com.dev.eficiente.nosso.mercado.livre.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.Setter;
+import br.com.dev.eficiente.nosso.mercado.livre.util.CryptPassword;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @Entity
+@Validated
 @Table(name = "user")
-public class User implements Serializable {
+public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,14 +30,12 @@ public class User implements Serializable {
 
     @NotNull
     @Past
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime instant;
 
     public User(@NotBlank @Email String login,
-                @NotBlank @Length(min = 6) String password,
-                @NotNull @Past @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss") LocalDateTime instant){
+                @NotBlank @Length(min = 6) String password){
         this.login = login;
-        this.password = password;
-        this.instant = instant;
+        this.password = new CryptPassword().encode(password);
+        this.instant = LocalDateTime.now();
     }
 }
